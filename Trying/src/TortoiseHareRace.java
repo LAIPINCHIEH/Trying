da16fe47c72e1b6bfbfb98d6f680f87a5267c580
LAIPINCHIEH <camp9636@gmail.com>
2014-07-05 16:06:24 +0800

    Update TortoiseHareRace.java

--- Trying/src/TortoiseHareRace.java
+++ Trying/src/TortoiseHareRace.java
@@ -4,7 +4,7 @@ public class TortoiseHareRace {
 	public static void main(String[] args) {
 		boolean[] flags = {true, false};
 		int totalStep = 10;
-		int tortoiseStep[]
+		System.out.println("yaya");
 	}
 
 }
